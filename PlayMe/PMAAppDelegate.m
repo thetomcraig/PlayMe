2dbd5dc7f37135c8e59d024a0bc56761a5fb04eb
Tom Craig <thetomcraig@PHILO.local>
2014-07-11 08:28:53 -0700

    All Window classes moved for app structure refactor

--- PlayMe/PMAAppDelegate.m
+++ PlayMe/PMAAppDelegate.m
@@ -4,7 +4,7 @@
 
 @synthesize iTunesController = _iTunesController;
 @synthesize menubarController = _menubarController;
-@synthesize artworkWindowController = _artworkWindowController;
+///@synthesize artworkWindowController = _artworkWindowController;
 
 //##############################################################################
 //Setting up the default settings for the app.  Because awakeFromNib is called
@@ -31,7 +31,7 @@
     //Initialize stuff.  ArtworkWindowController first so it can get the update
     //from the iTunesController's init method
     //--------------------------------------------------------------------------
-    _artworkWindowController = [[ArtworkWindowController alloc] init];
+    ///_artworkWindowController = [[ArtworkWindowController alloc] init];
     _iTunesController = [[ITunesController alloc] init];
     _menubarController = [[MenubarController alloc] init];
     
